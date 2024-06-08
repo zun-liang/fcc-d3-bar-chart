6f84896be8cd31bb09ec33cf9e62648dbd7ec21c
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-08 17:43:01 -0500

    update tooltip

--- main.js
+++ main.js
@@ -10,20 +10,31 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    const economicData = data.data;
-    const parsedData = economicData.map((d) => ({
-      date: new Date(d[0]),
-      value: d[1],
-    }));
+    const gdpData = data.data.map((d) => [new Date(d[0]), d[1]]);
+
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("id", "tooltip")
+      .attr("width", "40px")
+      .attr("height", "20px")
+      .style("position", "absolute")
+      .style("display", "none")
+      .style("background-color", "rgba(255, 255, 255, 0.8)")
+      .style("color", "black")
+      .style("padding", "10px")
+      .style("border-radius", "5px")
+      .style("box-shadow", "2px 2px 5px black")
+      .style("text-align", "center");
 
     const xScale = d3
       .scaleTime()
-      .domain(d3.extent(parsedData, (d) => d.date))
+      .domain(d3.extent(gdpData, (d) => d[0]))
       .range([margin.left, width - margin.left]);
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(parsedData, (d) => d.value)])
+      .domain([0, d3.max(gdpData, (d) => d[1])])
       .nice()
       .range([height - margin.bottom, margin.top]);
 
@@ -58,23 +69,43 @@ fetch(
     svg
       .append("g")
       .selectAll("rect")
-      .data(parsedData)
+      .data(gdpData)
       .enter()
       .append("rect")
-      .attr("x", (d) => xScale(d.date))
-      .attr("y", (d) => yScale(d.value))
-      .attr(
-        "width",
-        (width - margin.left - margin.right) / parsedData.length - 1
-      )
-      .attr("height", (d) => height - margin.bottom - yScale(d.value))
-      .attr("fill", "skyblue")
-      .on("mouseover", (e, d) => d3.select(this).attr("fill", "orange"))
-      .on("mouseout", (e, d) => d3.select(this).attr("fill", "skyblue"));
+      .attr("class", "bar")
+      .attr("x", (d) => xScale(d[0]))
+      .attr("y", (d) => yScale(d[1]))
+      .attr("data-date", (d, i) => d[0].toISOString().split("T")[0])
+      .attr("data-gdp", (d, i) => d[1])
+      .attr("width", (width - margin.left - margin.right) / gdpData.length - 1)
+      .attr("height", (d) => height - margin.bottom - yScale(d[1]))
+      .attr("fill", "brown")
+      .on("mouseover", (event, d) => {
+        d3.select(event.currentTarget).style("fill", "white");
+        tooltip
+          .style("display", "block")
+          .html(
+            `${d[0].toISOString().split("-")[0]} ${
+              d[0].toISOString().split("-")[1] === "01"
+                ? "Q1"
+                : d[0].toISOString().split("-")[1] === "04"
+                ? "Q2"
+                : d[0].toISOString().split("-")[1] === "07"
+                ? "Q3"
+                : "Q4"
+            }<br/>$${d[1]} Billion`
+          )
+          .attr('data-date', d[0].toISOString().split("T")[0])
+          .style("left", `${event.pageX + 20}px`)
+          .style("bottom", "250px");
+      })
+      .on("mouseout", (event, d) => {
+        d3.select(event.currentTarget).style("fill", "brown");
+        tooltip.style("display", "none");
+      });
 
     svg
       .append("text")
-      .attr("class", "x label")
       .attr("x", width / 2)
       .attr("y", height - 30)
       .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf")
@@ -82,7 +113,6 @@ fetch(
 
     svg
       .append("text")
-      .attr("class", "y label")
       .attr("transform", "rotate(-90)")
       .attr("x", -height / 2)
       .attr("y", 90)
